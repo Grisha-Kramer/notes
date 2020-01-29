1eedfbebfa638aa6411f7fe8c5d540197ad37328
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-01-29 14:22:27 -0500

    updating html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,10 @@ var path = require("path");
 
 module.exports = function(svr) {
   svr.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../Develop/notes.html"));
+    res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
   });
+
+  svr.get("/index", function(req, res) {
+      res.sendFile(path.join(__dirname, "../Develop/public/index.html"))
+  })
 };
