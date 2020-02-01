1a001d98d5c786f10e9d3e31e335a9ea433aef64
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-02-01 12:35:52 -0500

    Finally connects properly

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,14 +2,14 @@ var path = require("path");
 
 module.exports = function(app) {
   app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+    res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
   });
 
   app.get("/index", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/index.html"))
+      res.sendFile(path.join(__dirname, "../Develop/public/index.html"))
   });
 
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
   });
 };
